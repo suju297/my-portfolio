5f26f752c09d60e9dd613a5de89491612cba1cd7
Sujendra Gharat <sgharat297@gmail.com>
2024-01-02 17:48:52 -0500

    ++ Boiler Plate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import Portfolio from './components/Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
